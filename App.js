e780d9090154c30ee259d607efc54d926e668418
Muhammad Salman Ahmed <salman.ahmed.9373@gmail.com>
2022-10-19 12:02:51 +0500

    2 Pages created successfully

--- App.js
+++ App.js
@@ -1,211 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ToastAndroid } from 'react-native';
+// React screen navigator Code
 import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from "./Login";
+import Signup from "./Signup";
+const Stack = createStackNavigator();
 
-export default function App() {
-
-  const [name, SetName] = useState('');
-  const [submitted, SetSubmitted] = useState(false);
-  const[password, SetPassword] = useState(false);
-  const onPressHandler = () => {
-    if (name.length == 11) {
-      SetSubmitted(!submitted);
-    } else {
-      // Alert.alert(
-      //   'Warning',
-      //   'The name must be longer than 3 characters', [
-      //   {
-      //     text: 'Do not show again',
-      //     onPress: () => console.warn('Do not show Pressed!')
-      //   },
-      //   {
-      //     text: 'Cancel',
-      //     onPress: () => console.warn('Cancel Pressed!')
-      //   },
-      //   {
-      //     text: 'OK',
-      //     onPress: () => console.warn('OK Pressed!')
-      //   },
-      // ],
-      //   {
-      //     cancelable: true,
-      //     onDismiss: () => console.warn('Alert dismissed!')
-      //   })
-      ToastAndroid.showWithGravity(
-        'The Number must consist of 11 characters',
-        ToastAndroid.LONG,
-        ToastAndroid.CENTER,
-      )
-    }
-
-    if(password.length == 8){
-      SetPassword(!password)
-    } else{
-      ToastAndroid.showWithGravity(
-        'Your password is too short',
-        ToastAndroid.SHORT,
-        ToastAndroid.BOTTOM
-      )
-    }
-
-    if(name.length==0 && password.length==0){
-      ToastAndroid.showWithGravity(
-        'Fields can not be empty',
-        ToastAndroid.LONG,
-        ToastAndroid.BOTTOM,
-      )
-    }
-
-    if(name.length!=0 && password.length==0){
-      ToastAndroid.showWithGravity(
-        'Password is Required',
-        ToastAndroid.LONG,
-        ToastAndroid.BOTTOM,
-      )
-    }
-
-    if(name.length<11){
-      ToastAndroid.showWithGravity(
-        'Your number must consist of 11 characters',
-        ToastAndroid.LONG,
-        ToastAndroid.BOTTOM,
-      )
-    }
+export default function App(props) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          // screenOptions={{
+          //   header: () => null
+          // }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={Login}
+          // options={{
+          //   header: () => null
+          // }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   }
-
-  return (
-
-
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      <View style={styles.iconview}>
-
-        {/* This is a View where only icon is Placed */}
-        <Image
-          style={styles.tinyLogo}
-          source={require('../blooddonation/assets/bdi.png')}
-        />
-      </View>
-
-      <View style={styles.txtinputview}>
-        {/* <Text>2</Text>  This is the View where Only text Input is placed*/}
-        <TextInput
-          style={styles.input}
-          // onChangeText={(value) => setName(value)}
-
-          placeholder="Enter Cell No."
-          onChangeText={(value) => SetName(value)}
-          keyboardType="numeric"
-          maxLength={11}
-        />
-        {/* <Text> Your Cell Number is : {name}</Text> */}
-
-        <TextInput
-          style={styles.input}
-          // onChangeText={onChangeNumber}
-          // value={number}
-          placeholder="Enter Password"
-          keyboardType="default"
-          onChangeText={(value) => SetPassword(value)}
-          maxLength={8}
-          secureTextEntry
-        />
-
-        <TouchableOpacity style={styles.opacityforgot}>
-          <Text style={styles.txtcolor}>Forgot Password?</Text>
-        </TouchableOpacity>
-
-      </View>
-
-      <View style={styles.btnview}>
-        {/* This is the View where only buttons are placed */}
-        <TouchableOpacity style={styles.buttons} onPress={onPressHandler}>
-          <Text style={styles.btntxtcolor}>Login</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttons}>
-          <Text style={styles.btntxtcolor}>Become a Donor</Text>
-        </TouchableOpacity>
-
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  buttons:{
-    backgroundColor:'red',
-    width:200,
-    margin:8,
-    height:50,
-    borderRadius:20,
-    alignItems:"center",
-    justifyContent:"center",
-    marginBottom:10
-  },
-  btntxtcolor:{
-    color:"white",
-    alignItems:"center",
-    justifyContent:"center",
-    fontSize:20,
-    fontWeight:"bold"
-  },
-  iconview: {
-    flex: 1.2,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  txtinputview: {
-    flex: 1.5,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  btnview: {
-    flex: 1.5,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    alignItems: "center",
-  },
-  tinyLogo: {
-    width: 150,
-    height: 150,
-    borderRadius: 150
-  },
-  txtcolor: {
-    color: "#ff0000"
-  },
-  input: {
-    backgroundColor: 'white',
-    color: "#555",
-    width: 200,
-    alignItems: 'center',
-    margin: 10,
-    justifyContent: 'center',
-    borderRadius: 8,
-    height: 30,
-    borderWidth: 1,
-    textAlign: 'center'
-  },
-  opacityforgot: {
-    backgroundColor: "white",
-    // color:"#ff0000",
-    alignItems: "center",
-    marginLeft:200,
-    marginTop:30,
-    width:140,
-    height:30,
-    justifyContent: 'center',
-    borderRadius:8,
-  }
-});
--- Signup.js
+++ Signup.js
@@ -4,16 +4,186 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React, { useState } from "react";
 
-export default function Signup() {
+export default function Signup({ navigation }) {
 
-    return (
+  const [submitted, SetSubmitted] = useState(false);
+  const [name, SetName] = useState('');
+  const [num, SetNum] = useState('');
+  const [bgrp, SetBgrp] = useState('');
+  const [city, SetCity] = useState('');
+  const [address, SetAddress] = useState('');
+  const [password, SetPassword] = useState('');
+
+  const onPressHandler = () => {
+
+    if(name.length ==0)
+  {
+    ToastAndroid.showWithGravity(
+      'The Name field is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+  }
+
+  if (password.length == 0)
+  {
+    ToastAndroid.showWithGravity(
+      'The Password is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+    
+  }
+
+  // if(password.length !=0 && password.length <=7)
+  // {
+  //   ToastAndroid.showWithGravity(
+  //     'The Password must consist of atleast 8  characters',
+  //     ToastAndroid.LONG,
+  //     ToastAndroid.BOTTOM,
+  //   )
+  // }
+
+  if(num.length==0)
+  {
+    ToastAndroid.showWithGravity(
+      'The Number is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+  }
+
+  // if(num.length!=0 && num.length <=10)
+  // {
+  //   ToastAndroid.showWithGravity(
+  //     'The NUmber Must consist of 11 characters',
+  //     ToastAndroid.LONG,
+  //     ToastAndroid.BOTTOM,
+  //   )
+  // }
+  
+
+  if(bgrp.length == 0)
+  {
+    ToastAndroid.showWithGravity(
+      'The Blood Group field is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+  }
+  // if(bgrp.length !=0 && bgrp.length<=1)
+  // {
+  //   ToastAndroid.showWithGravity(
+  //     'The Blood Group must be written in this format i.e A+',
+  //     ToastAndroid.LONG,
+  //     ToastAndroid.BOTTOM,
+  //   )
+  // }
+
+  if(city.length == 0)
+  {
+    ToastAndroid.showWithGravity(
+      'The City field is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+  }
+
+  if(address.length == 0)
+  {
+    ToastAndroid.showWithGravity(
+      'The address is required',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    )
+  }
+
+  }
+
+  
+  return (
 
 
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-            <Text> Hello I am working on page 2</Text>
-        </View>
-      );
+    <View style={styles.container}>
+      <View style={styles.view1}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('../blooddonation/assets/bdi.png')}
+        />
+      </View >
+      <View style={styles.view2}>
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="Full Name"
+          onChangeText={(value) => SetName(value)}
+          keyboardType="default"
+          maxLength={25}
+        />
+
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="Mobile Number"
+          onChangeText={(value) => SetNum(value)}
+          keyboardType="numeric"
+          maxLength={11}
+        />
+
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="Blood Group i.e A+"
+          onChangeText={(value) => SetBgrp(value)}
+          keyboardType="default"
+          maxLength={2}
+        />
+
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="City"
+          onChangeText={(value) => SetCity(value)}
+          keyboardType="default"
+          maxLength={30}
+        />
+
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="Address"
+          onChangeText={(value) => SetAddress(value)}
+          keyboardType="default"
+          maxLength={50}
+        />
+
+        <TextInput
+          style={styles.input}
+          // onChangeText={(value) => setName(value)}
+
+          placeholder="Password"
+          onChangeText={(value) => SetPassword(value)}
+          keyboardType="default"
+          maxLength={20}
+        />
+      </View>
+
+
+
+      <View style={styles.view3}>
+        <TouchableOpacity style={styles.buttons}
+        onPress={onPressHandler}
+        >
+          <Text style={styles.btntxtcolor}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 
 
 
@@ -21,10 +191,64 @@ export default function Signup() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'stretch',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "stretch",
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    // backgroundColor:"blue",
+    alignItems: "center",
+    justifyContent: "center"
+
+  },
+  view2: {
+    flex: 1.5,
+    // backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  view3: {
+    flex: 0.5,
+    // backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center"
+
+  },
+  tinyLogo: {
+    width: 150,
+    height: 150,
+    borderRadius: 150
+  },
+  input: {
+    backgroundColor: 'white',
+    color: "#555",
+    width: 200,
+    alignItems: 'center',
+    margin: 10,
+    justifyContent: 'center',
+    borderRadius: 8,
+    height: 30,
+    borderWidth: 1,
+    textAlign: 'center'
+  },
+  buttons:{
+    backgroundColor:'red',
+    width:200,
+    margin:8,
+    height:50,
+    borderRadius:20,
+    alignItems:"center",
+    justifyContent:"center",
+    marginBottom:10
+  },
+  btntxtcolor:{
+    color:"white",
+    alignItems:"center",
+    justifyContent:"center",
+    fontSize:20,
+    fontWeight:"bold"
+  },
 });
