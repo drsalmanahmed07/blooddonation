aedf7be52d875ad7adf490d28394d526d879b22d
Muhammad Salman Ahmed <salman.ahmed.9373@gmail.com>
2022-10-23 22:42:41 +0500

    I have made a new Page

--- Requestblood.js
+++ Requestblood.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ToastAndroi
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React, { useState } from "react";
+// import { Picker } from 'react-native-picker/picker';
 
 export default function Requestblood({ navigation }) {
 
@@ -16,12 +17,52 @@ export default function Requestblood({ navigation }) {
             </View>
 
             <View style={styles.view2}>
-                <Text style = {styles.heading}> Blood Group:</Text>
-                
+                <Text style={styles.heading}> Blood Group:</Text>
+                {/* <Picker style = {styles.picker}>
+                <Picker.Item label = "A +ive" value = "A Positive"/>
+                </Picker>
+                 */}
+                <TextInput
+                    style={styles.input}
+                    // onChangeText={(value) => setName(value)}
+                    placeholder=" Enter Blood Group i.e  B +ive"
+                    onChangeText={(value) => SetName(value)}
+                    keyboardType="numeric"
+                    maxLength={11}
+                />
+
+                <Text style={styles.heading}> Address:</Text>
+                {/* <Picker style = {styles.picker}>
+                <Picker.Item label = "A +ive" value = "A Positive"/>
+                </Picker>
+                 */}
+                <TextInput
+                    style={styles.input}
+                    // onChangeText={(value) => setName(value)}
+                    placeholder=" Your Address Here"
+                    // onChangeText={(value) => SetName(value)}
+                    keyboardType="default"
+                    maxLength={5}
+                />
+                <Text style={styles.heading}>Description * :</Text>
+                {/* <Picker style = {styles.picker}>
+                <Picker.Item label = "A +ive" value = "A Positive"/>
+                </Picker>
+                 */}
+                <TextInput
+                    style={styles.input}
+                    // onChangeText={(value) => setName(value)}
+                    placeholder="Description (Optional)"
+                    onChangeText={(value) => SetName(value)}
+                    keyboardType="numeric"
+                    maxLength={11}
+                />
             </View>
 
             <View style={styles.view3}>
-                <Text> This is View 1</Text>
+                <TouchableOpacity style={styles.buttons}>
+                    <Text style={styles.btntxtcolor}>Post Request</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -36,6 +77,44 @@ const styles = StyleSheet.create({
         alignItems: "stretch",
         justifyContent: 'center',
     },
+    btntxtcolor: {
+        color: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    buttons: {
+        backgroundColor: 'red',
+        width: 200,
+        margin: 8,
+        marginTop:25,
+        height: 50,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 10
+    },
+    input: {
+        backgroundColor: 'white',
+        color: "#555",
+        width: 200,
+        alignItems: 'center',
+        marginTop: 10,
+        marginLeft: 80,
+        justifyContent: 'center',
+        borderRadius: 8,
+        borderColor: 'red',
+        height: 30,
+        borderWidth: 1,
+        textAlign: 'center'
+    },
+    picker: {
+        width: 300,
+        height: 45,
+        borderColor: 'red',
+        borderWidth: 5
+    },
     view1: {
         flex: 1,
         // backgroundColor: 'red',
@@ -45,8 +124,8 @@ const styles = StyleSheet.create({
     },
 
     view2: {
-        flex: 1,
-        backgroundColor: 'yellow',
+        flex: 1.5,
+        // backgroundColor: 'yellow',
         // alignItems: 'center',
         // justifyContent: 'center'
 
@@ -54,21 +133,23 @@ const styles = StyleSheet.create({
 
     view3: {
         flex: 1,
-        backgroundColor: 'green',
+        // backgroundColor: 'green',
         alignItems: 'center',
-        justifyContent: 'center'
+        // justifyContent: 'center',
+      
 
     },
     tinyLogo: {
         width: 150,
         height: 150,
         borderRadius: 150
-      },
-      heading:{
-        fontSize:18,
-        fontWeight:'800',
+    },
+    heading: {
+        fontSize: 18,
+        fontWeight: '800',
         marginLeft: 25,
-        
-      },
+        marginTop: 10
+
+    },
 });
 
